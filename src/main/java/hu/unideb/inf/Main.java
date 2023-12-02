d4b9cb0e2e35dac5b4edebc854936fc82532bf1a
Varga Petra <vargapetra0123@gmail.com>
2023-12-02 05:09:52 +0100

    Initial commit

---
+++ src/main/java/hu/unideb/inf/Main.java
@@ -0,0 +1,12 @@
+package hu.unideb.inf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class,args);
+
+    }
+}
